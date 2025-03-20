fc797cbeebf0372008cb7f423c3197291cb03a14
areufluk <chanayut.ja@gmail.com>
2025-03-21 00:20:10 +0700

    change dim_city location

--- scripts/dim_city.py
+++ scripts/dim_city.py
@@ -29,3 +29,38 @@ def create_spark_session(app_name="DefaultApp"):
         builder = builder.config(key, value)
 
     return builder.getOrCreate()
+
+
+def transform_dim_city():
+    bucket_name = "e-commerce-data-platform"
+    source_file_path = f"gs://{bucket_name}/raw/cities.csv"
+    destination_file_path = f"gs://{bucket_name}/transform/city"
+
+    spark = create_spark_session(
+        app_name="Transform dim city"
+    )
+
+    # Read csv from raw storage
+    df = spark.read \
+        .option("inferSchema", True) \
+        .option("header", True) \
+        .csv(source_file_path)
+
+    # Change column name
+    df = df \
+        .withColumnRenamed("CityID", "id") \
+        .withColumnRenamed("CityName", "city_name") \
+        .withColumnRenamed("Zipcode", "zipcode") \
+        .withColumnRenamed("CountryID", "country_id")
+
+    # Write parquet to transform storage
+    df.write \
+        .mode("overwrite") \
+        .parquet(destination_file_path)
+
+    # Stop the Spark session
+    spark.stop()
+
+
+if __name__ == "__main__":
+    transform_dim_city()
